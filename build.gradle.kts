9dc1e3dc8525766309ac592da431d57cbe12883d
Ryan Ashiruma <ryananyangu@gmail.com>
2022-10-18 16:44:30 +0300

    docker push added to workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,24 +50,24 @@ tasks.jacocoTestReport {
     }
 }
 
-// tasks.jacocoTestCoverageVerification {
-//     violationRules {
-//         rule {
-//             limit {
-//                 minimum = "0.5".toBigDecimal()
-//             }
-//         }
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.5".toBigDecimal()
+            }
+        }
 
-//         rule {
-//             isEnabled = false
-//             element = "CLASS"
-//             includes = listOf("org.gradle.*")
+        rule {
+            isEnabled = false
+            element = "CLASS"
+            includes = listOf("org.gradle.*")
 
-//             limit {
-//                 counter = "LINE"
-//                 value = "TOTALCOUNT"
-//                 maximum = "0.3".toBigDecimal()
-//             }
-//         }
-//     }
-// }
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "0.3".toBigDecimal()
+            }
+        }
+    }
+}
