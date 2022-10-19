6d8039d0dd43c5960967af61f2a8e5600d344d28
Ryan Ashiruma <ryananyangu@gmail.com>
2022-10-19 17:10:39 +0300

    setting up web

---
+++ src/main/kotlin/tech/zohari/ktgradle/controllers/HelloWorld.kt
@@ -0,0 +1,19 @@
+package tech.zohari.ktgradle.controllers
+
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+  @GetMapping("/")
+  fun blog(model: Model): String {
+    model["title"] = "Hello Ryan"
+    return "hello"
+  }
+
+}
+
