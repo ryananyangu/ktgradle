ee49b230786971f1786159d496db8beafdbb388a
Ryan Ashiruma <ryananyangu@gmail.com>
2022-10-21 08:59:24 +0300

    Working moustache

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("com.h2database:h2")
